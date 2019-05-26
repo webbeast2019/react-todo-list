e0188ae985cc847688f4d8f997a3c95232f691b5
yuvalbl <yuval.bar.levi@gmail.com>
2019-05-26 19:49:51 +0300

    use state hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,27 +4,12 @@ interface IProps {
   onAdd: Function
 }
 
-// this.state = {
-//   value: '',
-//   buttonEnabled: false
-// };
-
-// const handleChange = (event: SyntheticEvent) => {
-//   const value = (event.target as HTMLInputElement).value;
-//   this.setState({
-//     value,
-//     buttonEnabled: (value.length > 0)
-//   });
-// };
-
-// class Header extends React.Component<IProps, IState> {
 const Header: React.FC<IProps> = ({onAdd}) => {
   const [value, setValue] = useState('');
   const [buttonEnabled, setButtonEnabled] = useState(false);
   
   const handleChange = (event: SyntheticEvent) => {
     const newValue = (event.target as HTMLInputElement).value;
-    console.log(newValue);
     setValue(newValue);
     setButtonEnabled(newValue.length > 0);
   };
